d41e07ed861ae29a3e676e1f419cd091ecd680a9
Vegadelalyra <lyradeperseo@gmail.com>
2024-06-02 03:03:59 -0500

    feat[sprint_boot]: Create students CRUD on Spring Boot on three-layer architecture and a h2 postgres mem DB!

---
+++ src/main/java/com/lyradeperseo/springboot_new_leaf/SpringbootNewLeafApplication.java
@@ -0,0 +1,15 @@
+package com.lyradeperseo.springboot_new_leaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootNewLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNewLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyradeperseo/springboot_new_leaf/SpringbootNewLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyradeperseo.springboot_new_leaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNewLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
